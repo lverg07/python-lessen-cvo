2b886ea1d9db05f4a26ee13da38bc42e5ad6be35
Lobke Vergauwen <lverg2007@gmail.com>
2026-05-29 11:05:47 +0200

    OefC deel 1 klaar

---
+++ les2_oefC.py
@@ -0,0 +1,14 @@
+getal_1 = int(input("Voer het eerste gehele getal in:"))
+getal_2 = int(input("Voet het tweede gehele getal in:"))
+
+# Berekening
+
+som = getal_1 + getal_2
+verschil = getal_1 - getal_2
+product = getal_1 * getal_2
+
+# Resultaat
+
+print(f"Som: {som}")
+print(f"Verschil: {verschil}")
+print(f"Product: {product}")
